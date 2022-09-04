779d058537ffc22eda7a96d2d1ee30e48593d3b8
hgbaek <hgbaek@seegene.com>
2022-09-04 20:05:16 +0900

    [feature] Implement join video call screen UI part1.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:calandiva/resources/auth_methods.dart';
 import 'package:calandiva/utils/colors.dart';
 import 'package:calandiva/views/home_screen.dart';
 import 'package:calandiva/views/login_screen.dart';
+import 'package:calandiva/views/video_call_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import 'swipe_feed_page.dart';
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => const LoginScreen(),
         '/home': (context) => const HomeScreen(),
+        '/video-call': (context) => const VideoCallScreen(),
       },
       home: StreamBuilder(
         stream: AuthMethods().authChanges,
---
+++ lib/views/video_call_screen.dart
@@ -0,0 +1,96 @@
+import 'package:calandiva/resources/auth_methods.dart';
+import 'package:calandiva/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class VideoCallScreen extends StatefulWidget {
+  const VideoCallScreen({Key? key}) : super(key: key);
+
+  @override
+  State<VideoCallScreen> createState() => _VideoCallScreenState();
+}
+
+class _VideoCallScreenState extends State<VideoCallScreen> {
+  final AuthMethods _authMethods = AuthMethods();
+
+  late TextEditingController meetingIdController;
+  late TextEditingController nameController;
+
+  @override
+  void initState() {
+    meetingIdController = TextEditingController();
+    nameController = TextEditingController(
+      text: _authMethods.user.displayName,
+    );
+    super.initState();
+  }
+
+  _joinMeeting() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: backgroundColor,
+        title: const Text(
+          'Join a Meeting',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 60,
+            child: TextField(
+              controller: meetingIdController,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                fillColor: secondaryBackgroundColor,
+                filled: true,
+                border: InputBorder.none,
+                hintText: 'Room ID',
+                contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 60,
+            child: TextField(
+              controller: nameController,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                fillColor: secondaryBackgroundColor,
+                filled: true,
+                border: InputBorder.none,
+                hintText: 'Name',
+                contentPadding: EdgeInsets.fromLTRB(16, 8, 0, 0),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: _joinMeeting,
+            child: const Padding(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                'join',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
